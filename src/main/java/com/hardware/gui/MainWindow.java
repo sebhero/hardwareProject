81e3859695defd318c533b05ff4c18a49137a952
Coweete <Jonatan_fridsten@hotmail.com>
2016-04-12 12:11:45 +0200

    All classes is up

---
+++ src/main/java/com/hardware/gui/MainWindow.java
@@ -0,0 +1,15 @@
+package com.hardware.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by jonatan on 2016-04-12.
+ */
+public class MainWindow extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/hardware/piController/PiController.java
@@ -0,0 +1,7 @@
+package com.hardware.piController;
+
+/**
+ * Created by jonatan on 2016-04-12.
+ */
+public class PiController {
+}
